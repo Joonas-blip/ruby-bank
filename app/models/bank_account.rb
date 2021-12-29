01f5d3fc53f6e59313c6bfd67c7bb20a942cde8e
Joonas Villanen <joonas.villa@gmail.com>
2021-12-29 13:11:13 +0200

    Add seeds

--- app/models/bank_account.rb
+++ app/models/bank_account.rb
@@ -1,5 +1,5 @@
 class BankAccount < ApplicationRecord
-  before_validation :set_default_balance
+  before_validation :set_default_balance, :new_account_number
   belongs_to :user
   has_many :transactions
   validates :account_number, presence: true, uniqieness: true
@@ -8,6 +8,12 @@ class BankAccount < ApplicationRecord
   def set_default_balance
     if self.new_record?
       self.balance = 0.00
+    end
+  end
+
+  def new_account_number
+    if self.new_record?
+      self.account_number = "NL01RUBY#{10.times.map{rand(10)}.join}"
     end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# destroy all users
+User.destroy_all
+BankAccount.destroy_all
+
+user_1 = User.create(email: 'User1@mail.com', password: '123456', full_name: 'John Doe')
+user_2 =User.create(email: 'User2@mail.com', password: '123456', full_name: 'Andrea Corbetta')
+admin = User.create(email: 'admin@mail.com',  password: '123456', full_name: 'Davide Oldani', admin: true)
+
+BankAccount.create(user: user_1, account_number: 'NL01RUBY9243483368')
+BankAccount.create(user: user_2, account_number: 'NL01RUBY7339065790')
+BankAccount.create(user: admin, account_number: 'NL01RUBY0000000000')
