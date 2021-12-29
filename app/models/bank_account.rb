f4cb03b4cd65fa3dd721e24d7b71f9d8fd90883f
Joonas Villanen <joonas.villa@gmail.com>
2021-12-29 11:28:24 +0200

    model validations added

--- app/models/bank_account.rb
+++ app/models/bank_account.rb
@@ -1,3 +1,13 @@
 class BankAccount < ApplicationRecord
+  before_validation :set_default_balance
   belongs_to :user
+  has_many :transactions
+  validates :account_number, presence: true, uniqieness: true
+  validates :user, presence: true
+
+  def set_default_balance
+    if self.new_record?
+      self.balance = 0.00
+    end
+  end
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,2 +1,11 @@
 class Transaction < ApplicationRecord
+  before_validation :set_transaction_number
+  belongs_to :bank_account
+
+  validates :amount, presence: true, numericality: true
+  validates :transaction_number, presence: true, uniqueness: true
+
+  def set_transaction_number
+    self.transaction_number = SecureRandom.uuid
+  end
 end
