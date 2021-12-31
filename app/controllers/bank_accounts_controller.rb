b56a5373dc901bd1a4bacdff6131385d2941b0fb
Joonas Villanen <joonas.villa@gmail.com>
2021-12-31 12:20:49 +0200

    Can create new accounts

--- app/controllers/bank_accounts_controller.rb
+++ app/controllers/bank_accounts_controller.rb
@@ -3,6 +3,12 @@ class BankAccountsController < ApplicationController
   def index
     @current_user = current_user
     @bank_accounts = @current_user.bank_accounts
+    @new_account = BankAccount.new
   end
 
+  def create
+    account = BankAccount.new(user: current_user)
+    account.save
+    redirect_to bank_accounts_path
+  end
 end
